74db012e47dfdd16ad2d692043310fac1d861c20
Arun Singh <singh16.arun@gmail.com>
2016-05-14 13:59:45 -0700

    rename

--- AdjacencyListTable.sql
+++ AdjacencyListTable.sql
@@ -4,6 +4,6 @@ CREATE TABLE `tree_nodes` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
   `parent_id` int(11) DEFAULT NULL,
-  `hierarchy_level` int(11) DEFAULT NULL,
+  `level` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
 ) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=latin1;
