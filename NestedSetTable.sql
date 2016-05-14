b2a3aed8bd26ff97fff2120ccb63f08bb1e64535
Arun Singh <singh16.arun@gmail.com>
2016-05-14 13:56:27 -0700

    rename

--- NestedSetTable.sql
+++ NestedSetTable.sql
@@ -1,10 +1,10 @@
 -- Target table holding the nested set hierarchy.
 
-CREATE TABLE `catalog_tree` (
+CREATE TABLE `tree` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL DEFAULT '',
   `parent_id` int(11) DEFAULT NULL,
-  `hierarchy_level` int(11) DEFAULT NULL,
+  `level` int(11) DEFAULT NULL,
   `lft` int(11) NOT NULL,
   `rgt` int(11) DEFAULT NULL,
   `stack_top` int(11) NOT NULL,
