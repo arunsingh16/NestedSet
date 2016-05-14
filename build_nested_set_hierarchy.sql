7c43f401f0f57a5e5ef922262d881d86007e7410
Arun Singh <singh16.arun@gmail.com>
2016-05-14 13:59:12 -0700

    rename

--- build_nested_set_hierarchy.sql
+++ build_nested_set_hierarchy.sql
@@ -1,44 +1,44 @@
 -- Stored procedure for building the Nested Set from adjacency list.
 
 DELIMITER ;;
-CREATE DEFINER=`spf_root`@`%` PROCEDURE `build_nested_hierarchy`()
+CREATE DEFINER=`test_user`@`%` PROCEDURE `build_nested_hierarchy`()
 BEGIN
 DECLARE counter INT;
 DECLARE max_counter INT;
 DECLARE current_top INT;
 
 SET counter = 2;
-SET max_counter = 2 * (SELECT COUNT(*) FROM catalog_tree_nodes);
+SET max_counter = 2 * (SELECT COUNT(*) FROM tree_nodes);
 SET current_top = 1;
 
-INSERT INTO catalog_tree (stack_top, id , name, parent_id, hierarchy_level, lft, rgt)
-SELECT 1, id, name, parent_id, hierarchy_level, 1, NULL
-  FROM catalog_tree_nodes
+INSERT INTO tree (stack_top, id , name, parent_id, level, lft, rgt)
+SELECT 1, id, name, parent_id, level, 1, NULL
+  FROM tree_nodes
  WHERE parent_id = 0;
 
-DELETE FROM catalog_tree_nodes
+DELETE FROM tree_nodes
  WHERE parent_id = 0;
 	
 WHILE counter <= (max_counter - 2) DO
-	select @count := count(*) FROM catalog_tree AS CT, catalog_tree_nodes AS CTN WHERE CT.id = CTN.parent_id AND CT.stack_top = current_top;
+	select @count := count(*) FROM tree AS CT, tree_nodes AS TN WHERE CT.id = TN.parent_id AND CT.stack_top = current_top;
 	IF @count <= 0 THEN
-		    UPDATE catalog_tree
+		    UPDATE tree
           	SET rgt = counter,
             stack_top = -stack_top -- pops the stack
         	WHERE stack_top = current_top;
     	   	SET counter = counter + 1;
 	       	SET current_top = current_top - 1;
 	ELSE
-			INSERT INTO catalog_tree (stack_top, id , name, parent_id, hierarchy_level, lft, rgt)
-       		SELECT (current_top + 1), CTN.id, CTN.name, CTN.parent_id, CTN.hierarchy_level, counter, NULL
-         	FROM catalog_tree AS CT, catalog_tree_nodes AS CTN
-        	WHERE CT.id = CTN.parent_id
+			INSERT INTO tree (stack_top, id , name, parent_id, level, lft, rgt)
+       		SELECT (current_top + 1), TN.id, TN.name, TN.parent_id, TN.level, counter, NULL
+         	FROM tree AS CT, tree_nodes AS TN
+        	WHERE CT.id = TN.parent_id
           	AND CT.stack_top = current_top
           	LIMIT 1;
 
-        	DELETE FROM catalog_tree_nodes
+        	DELETE FROM tree_nodes
          	WHERE id = (SELECT id
-                        FROM catalog_tree
+                        FROM tree
                        WHERE stack_top = current_top + 1);
 
         	SET counter = counter + 1;
